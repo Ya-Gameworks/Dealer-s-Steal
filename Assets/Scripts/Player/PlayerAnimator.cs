02fdba390835ed9a8de50fac8eed249222e08c87
CantFind <cinarguvencli@hotmail.com>
2024-11-02 14:56:04 +0300

    Anim Start

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    
+    public Animator PlayerAnims;
+
+    private void Update()
+    {
+        PlayerAnims.SetBool("Jump", PlayerMovement.Instance.IsJumping || PlayerMovement.Instance.IsPogoJumping);
+        PlayerAnims.SetVector("Vel Y", new Vector2(0,PlayerMovement.Instance.RB.linearVelocityY));
+        
+    }
 }
